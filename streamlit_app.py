3edf7f59500f171a59b50294ee431d973db93515
chirudeakin <candsbro@gmail.com>
2022-08-26 15:41:48 +1000

    Initial commit

---
+++ streamlit_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas
+import numpy
+
+st.title("Streamlit Form Demo")
+#CREATING OUR FORM FIELDS
+with st.form("form1", clear_on_submit=True):
+    name = st.text_input("Enter full name")
+    email = st.text_input("Enter email")
+    message = st.text_area("Message")
+    age = st.slider("Enter your age", min_value = 10, max_value = 100)
+    st.write(age)
+    submit=st.form_submit_button("Submit this form")
